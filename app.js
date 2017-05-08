51c0439c2313cc84979615a3dcb0f8a70b2ddd70
richard <richard.trujillo.torres@gmail.com>
2017-05-08 22:33:02 +0200

    added api calls

--- app.js
+++ app.js
@@ -12,6 +12,7 @@
 })();
 
 
+
 /**
 * Main controller
 */
@@ -42,8 +43,8 @@
 		.module('app')
 		.controller('UserController', UserController);
 
-	UserController.$inject = ['$http', '$state'];
-	function UserController($http, $state) {
+	UserController.$inject = ['$http', '$state', 'dataservice'];
+	function UserController($http, $state, dataservice) {
 		var vm = this;
 		vm.profile = {};
 		vm.createProfile = createProfile;
@@ -55,12 +56,41 @@
 		function createProfile() {
 			console.log(vm.profile);
 
-			// craete profile
-			// move to voting section
+			var params = {
+				operation: 'create',
+				payload: {
+					"TableName": "user",
+				    "Item": {
+				        "sex": vm.profile.sex,
+				        "search": vm.profile.likes,
+				        "age": vm.profile.age
+			    	}
+				}
+			};
 
-			$state.go('photo');
+
+			// create profile
+			dataservice
+				.save('user', params)
+					.then(function success(data, headers, status, config) {
+						console.log(data);
+						// move to voting section
+						$state.go('photo', {
+							user: {
+								id: data.data,
+								sex: vm.profile.sex,
+								likes: vm.profile.likes,
+								age: vm.profile.age
+							}
+						});
+					}, function error(response) {
+						console.log('Something went wrong');
+						$state.go('main');
+					});
 
 		}
+
+		
 	}
 
 })();
@@ -78,24 +108,141 @@
 		.module('app')
 		.controller('PhotoController', PhotoController);
 
-	PhotoController.$inject = ['$http'];
-	function PhotoController($http) {
+	PhotoController.$inject = ['$http', '$state', '$stateParams', 'dataservice'];
+	function PhotoController($http, $state, $stateParams, dataservice) {
 		var vm = this;
+		// vm.answer = {};
+		vm.submitAnswer = submitAnswer;
+		vm.photos = {};
+		vm.photo = {};
+		vm.NUMBER_OF_PHOTOS = 20;
+		vm.votedPhotos = 0;
+		vm.user = $stateParams.user;
 
-		// console.log('Start counting');
+		// TODO: check user
 
-		startCounter();
+		console.log(vm.user);
+
+		loadPhotos();
+
+
+
+
+
+		function loadPhotos() {
+			console.log('loading photos...');
+
+			dataservice
+				.all('photo')
+					.then(function success(data, headers, status, config) {
+						console.log(data.data.Items);
+						// once loaded, start the counter
+						vm.photos = data.data.Items;
+						showNextPhoto();
+
+						startCounter();
+					}, function error(response) {
+						console.log('Something went wrong');
+						$state.go('main');
+					});
+
+			
+		}
+
+		function submitAnswer(answer) {
+
+			// stop counter
+			stopCounter();
+
+			// send answer
+			console.log(answer);
+
+			var params = {
+				"operation": "create",
+				"payload": {
+				    "TableName": "vote",
+				    "Item": {
+				        "userId": vm.user.id,
+				        "sex": vm.user.sex,
+				        "search": vm.user.likes,
+				        "age": vm.user.age,
+				        "photoId": vm.photo.id,
+				        "like": "0/1",
+				        "milliseconds": 100000000
+				    }
+				  }
+			};
+
+
+			// dataservice
+			// 	.save('vote', params)
+			// 		.then(function success(data, headers, status, config) {
+			// 			console.log(data);
+			// 			// move to voting section
+						
+			// 		}, function error(response) {
+			// 			console.log('Something went wrong');
+			// 			$state.go('main');
+			// 		});
+
+			// increase vote counter
+			vm.votedPhotos++;
+
+			// check for end-of-test
+			if (vm.votedPhotos === (vm.NUMBER_OF_PHOTOS - 1)) {
+					// end of test
+				$state.go('thanks');
+			}
+
+			showNextPhoto();
+		}
+
+		function showNextPhoto() {
+			console.log(vm.photos[vm.votedPhotos]);
+			vm.photo.path = vm.photos[vm.votedPhotos].path;
+		}
+
+
+
+
+		function startCounter() {
+			console.log('counter started');
+		}
+
+		function stopCounter() {
+			console.log('counter stopped');
+		}
+
+
+
+		
 	}
 
 
-	function startCounter() {
-		console.log('counter started');
-	}
+	
 
 	
 
 })();
 
+
+
+(function() {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('ThanksController', ThanksController);
+
+	function ThanksController() {
+		var vm = this;
+
+		console.log('ThanksController()');
+	}
+
+})();
+
+	
 
 
 /**
@@ -123,9 +270,12 @@
 
 		$stateProvider
 			.state('photo', {
-				url: '/photo',
+				url: '/photo/{user:json}',
 				templateUrl: 'templates/photo.html',
-				controller: 'PhotoController as photo' 
+				controller: 'PhotoController as photo',
+				params: {
+					user: null
+				} 
 			});
 
 		$stateProvider
@@ -158,6 +308,14 @@
 				url: '/finish',
 				templateUrl: 'templates/user/finish.html'
 			});
+
+		$stateProvider
+			.state('thanks', {
+				url: '/thanks',
+				templateUrl: 'templates/thanks.html',
+				controller: 'ThanksController as thanks'
+			});
+
 	}
 
 })(); 
---
+++ data.service.js
@@ -0,0 +1,48 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('app')
+		.service('dataservice', data);
+
+	data.$inject = ['$http'];
+	function data($http) {
+		var URL = 'https://y8ohrxiy96.execute-api.eu-west-1.amazonaws.com/prod';
+
+		return {
+			all: function(resourceUri) {
+				return $http.get(URL + '/' + resourceUri);
+			},
+			get: function(resourceUri, id) {
+				return $http.get(URL + resourceUri + "/" + id);
+			}, 
+			save: function(resourceUri, params) {
+
+				console.log(URL);
+
+				return ($http({
+					method: 	'POST',
+					url: 		URL + "/" + resourceUri,
+					headers: 	{ 
+						'Content-Type' : 'application/json',
+						'Access-Control-Allow-Origin': '*' 
+					},
+					data: 		params
+				}));
+			},
+			put: function(resourceUri, id, params) {
+				return $http({
+					method: 	'POST',
+					url: 		URL + resourceUri + "/" + id,
+					headers: 	{ 'Content-Type' : 'application/json' },
+					data: 		params
+					
+				});
+			},
+			destroy: function(resourceUri, id) {
+				return $http.delete(URL + resourceUri + "/" + id);
+			}
+		};
+	}
+
+})();
